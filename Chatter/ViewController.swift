34187a5f371cc83cf15391c9d7991e325bfb4715
davalcato <davalcato@yahoo.com>
2020-06-16 16:19:44 -0700

    added the @IBAction func hideText along with the UIView.animate(withDuration: 0.7 to toogle text

--- Chatter/ViewController.swift
+++ Chatter/ViewController.swift
@@ -11,6 +11,13 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var helloLabel: UILabel!
+    @IBOutlet weak var hideTextButton: UIButton!
+    
+    
+    // Creating a toogle for the text
+    var textVisible: Bool = true
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -18,6 +25,30 @@ class ViewController: UIViewController {
 
     @IBAction func startChat(_ sender: Any) {
         helloLabel.text = "Hello to Chatter user!"
+        
+    }
+    @IBAction func hideText(_ sender: Any) {
+        // Checking to see if the text is currently showing
+        if textVisible {
+            UIView.animate(withDuration: 0.7, animations: {
+                self.helloLabel.alpha = 0.0
+            }) { (completed) in
+                self.helloLabel.isHidden = true
+                self.hideTextButton.setTitle("Show text", for: .normal)
+            }
+            textVisible = false
+        } else {
+            // This is where we reverse the toogle
+            self.helloLabel.isHidden = false
+            
+            UIView.animate(withDuration: 0.7) {
+                self.helloLabel.alpha = 1.0
+            }
+            
+            self.hideTextButton.setTitle("Hide text", for: .normal)
+            textVisible = true
+            
+        }
         
     }
     
